c84b759f0f332d9feeb431fb14ee316335bec5a3
Roman Pavelka <roman.pavelka.asi@gmail.com>
2022-11-13 09:39:14 +0100

    Medidicine used

---
+++ meds.sh
@@ -0,0 +1,29 @@
+# Medicine used
+
+## Fever
+
+Paracetamol: Paralen 500 mg (pain and fever)
+
+Ibuprofenum: Brufen 400 (pain, fever, and inflammation)
+
+## Mucoactive agents
+
+> ...aid in the clearance of mucus or sputum from the upper and lower airways...
+
+Source: [en.wikipedia.org](https://en.wikipedia.org/wiki/Mucoactive_agent)
+
+Erdosteinum: Erdomed (225 mg, works great: mucolytic, anti-adhesive, anti-inflammatory)
+
+Acetylcysteinum: ACC LONG (600 mg, works: mucolytic)
+
+## Cough suppressants (antitussives)
+
+Butamirati citras: Sinecod (50 mg, works)
+
+## Decongestants
+
+Xylometatazoline: Otrivin (1 mg/1 ml, works)
+
+## Skin problems
+
+Acidum salicylicum, Fluorouracil: Verrumal (5 mg/g, 100 mg/g, warts removal)
